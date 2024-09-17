120b63524322c90f7a75ceabc5f50245a5547fd0
KovalevskaOlga <146865293+KovalevskaOlga@users.noreply.github.com>
2024-09-17 19:06:37 +0300

    hw1

---
+++ js/vars.js
@@ -0,0 +1,35 @@
+// 1
+let age = 14
+console.log(age)
+// 2
+const name = "Olha"
+console.log(name)
+// 3
+const isStudent = true
+console.log(isStudent)
+// 4
+const myString = "Забудь про те, хто ти є зараз і стань тим, ким хочеш бути"
+console.log(myString)
+// 5
+const myNumber = 78 + 10
+console.log(myNumber)
+// 6
+const myNull = null
+console.log(myNull)
+// 7
+let yourName = prompt("введіть своє ім'я")
+console.log(yourName)
+alert("Bітаю!" + yourName);
+// 8
+const accept = confirm("Ви підтверджуєте?");
+if (accept)
+    alert("Дякую за підтвердження!")
+else
+    alert("Дію відмінено!")
+// 9
+const warning = confirm("Дія є небезпечною! Чи підтверджуєте?")
+if (warning)
+    alert("Дякую за підтвердження!")
+else
+    alert("Дію відмінено!")
+
